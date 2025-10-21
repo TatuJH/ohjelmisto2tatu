aebc9d4e8d28b64b7a1827e41ee9b97763569398
TatuJH <tatu.helminen@metropolia.fi>
2025-10-21 10:23:06 +0300

    tehtävä 12

--- mod12/tehtava12.py
+++ mod12/tehtava12.py
@@ -4,6 +4,6 @@ import requests
 print(requests.get("https://api.chucknorris.io/jokes/random").json()["value"])
 
 #Tehtävä 12.2
-api_key = "76bdba90b61d5ce1cae1b662de662b97"
+api_key = "avain"
 paikkakunta = input("Anna paikkakunta: ")
 print(f'{requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={paikkakunta}&appid={api_key}&units=metric&lang=fi").json()["weather"][0]["description"].capitalize()}, {requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={paikkakunta}&appid={api_key}&units=metric&lang=fi").json()["main"]["temp"]}°C')
